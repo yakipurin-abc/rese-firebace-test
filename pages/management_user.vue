a6e1326b20e6660269c8b5852c2109c7988757f8
yakipurin-abc <k.nishida0425@gmail.com>
2021-11-03 14:25:40 +0900

    deproy

--- pages/management_user.vue
+++ pages/management_user.vue
@@ -1,50 +1,56 @@
 <template>
   <div class="management">
-<ManagementHeader></ManagementHeader>
+    <ManagementHeader></ManagementHeader>
     <div class="management-ttl">
       <h2>ユーザー管理画面</h2>
     </div>
-    <div class="user-add" v-if="$auth.loggedIn">
-      <form >
-        <label for="">追加</label>
-        <input type="text" v-model="name" placeholder="name" required />
-        <select name="role_id" id="role_id" v-model="newRoleId">
-          <option value="" >ロールを選んでください</option>
-          <option value="1" >管理者</option>
-          <option value="2">店長</option>
-          <option value="3">従業員</option>
-        </select>
+    <div v-if="$auth.loggedIn">
+      <div class="user-add" >
+        <form >
+          <label for="">追加</label>
+          <input type="text" v-model="name" placeholder="name" required />
+          <select name="role_id" id="role_id" v-model="newRoleId">
+            <option value="" >ロールを選んでください</option>
+            <option value="1" >管理者</option>
+            <option value="2">店長</option>
+            <option value="3">従業員</option>
+          </select>
 
-        <input type="password" v-model="password" placeholder="password" required />
-        <button type="submit">追加</button>
-      </form>
-    </div>
-    <div class="user-info" >
-        <table v-if="$auth.loggedIn">
-        <tr>
-          <th>名前</th>
-          <th>ロール</th>
-          <th>更新</th>
-          <th>削除</th>
-        </tr>
-        <tr v-for="user in users" :key="user.id">
-          <td class="user-name">
-            <input type="text" v-model="user.name">
-          </td>
-          <td class="user-email" >
-            <select name="role_id" id="role_id" v-model="user.role_id" >
-              <option :value="user.role_id">{{user.role.name}}</option>
-              <option value="1" v>管理者</option>
-              <option value="2">店長</option>
-              <option value="3">従業員</option>
-              </select>
+          <input type="password" v-model="password" placeholder="password" required />
+          <button type="submit">追加</button>
+        </form>
+      </div>
+      <div class="user-info" >
+        <table>
+          <tr>
+            <th>名前</th>
+            <th>ロール</th>
+            <th>更新</th>
+            <th>削除</th>
+          </tr>
+          <tr v-for="user in users" :key="user.id">
+            <td class="user-name">
+              <input type="text" v-model="user.name">
             </td>
-            <td><button v-if="user.id != 1" @click="updateUser(user.id, user.name, user.role_id)">更新</button>
-            </td>
-          <td><button v-if="user.id != 1" @click="userDelete(user.id)">削除</button></td>
-        </tr>
-      </table>
+            <td class="user-email" >
+              <select name="role_id" id="role_id" v-model="user.role_id" >
+                <option :value="user.role_id">{{user.role.name}}</option>
+                <option value="1" v>管理者</option>
+                <option value="2">店長</option>
+                <option value="3">従業員</option>
+                </select>
+              </td>
+              <td><button v-if="user.id != 1" @click="updateUser(user.id, user.name, user.role_id)">更新</button>
+              </td>
+            <td><button v-if="user.id != 1" @click="userDelete(user.id)">削除</button></td>
+          </tr>
+        </table>
+      </div>
     </div>
+    <div v-else　class="login-comment">
+      <h2>ログインしてください</h2>
+    </div>
+    
   </div>
 </template>
 <script>
@@ -120,6 +126,10 @@ export default {
 };
 </script>
 <style scoped>
+.login-comment{
+  text-align: center;
+  margin-top:100px;
+}
 .management{
   background-color: rgb(238, 237, 237);
   min-height:100vh;
